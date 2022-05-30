0215449a2aa18c4ffe70b6b3730f218d2777903b
ramdole <ramdole@amorepacific.com>
2022-05-31 08:50:21 +0900

    Initial commit

---
+++ src/main/java/com/example/catalogsservice/contorller/CatalogController.java
@@ -0,0 +1,49 @@
+package com.example.catalogsservice.contorller;
+
+import com.example.catalogsservice.dto.CatalogDto;
+import com.example.catalogsservice.jpa.CatalogEntity;
+import com.example.catalogsservice.service.CatalogService;
+import com.example.catalogsservice.vo.ResponseCatalog;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("catalog-service")
+@RestController
+@RequiredArgsConstructor
+public class CatalogController {
+
+  private final CatalogService catalogService;
+  private final Environment env;
+
+  @GetMapping("/heath_check")
+  public String status() {
+    return String.format("It's Working in User Service on Port %s.",
+        env.getProperty("local.server.port"));
+  }
+
+  @GetMapping("catalogs")
+  public ResponseEntity<List<ResponseCatalog>> getCatalogs() {
+    Iterable<CatalogEntity> users = catalogService.getAllCatalogs();
+    List<ResponseCatalog> result = new ArrayList<>();
+    users.forEach(user-> result.add(new ModelMapper().map(user, ResponseCatalog.class)));
+    return ResponseEntity.status(HttpStatus.OK).body(result);
+  }
+
+  @GetMapping("catalogs/{productId}")
+  public ResponseEntity<ResponseCatalog> getCatalog(@PathVariable String productId) {
+    CatalogDto catalogDto = catalogService.getCatalogByProductId(productId);
+    ResponseCatalog responseCatalog = new ModelMapper().map(catalogDto, ResponseCatalog.class);
+
+    return ResponseEntity.status(HttpStatus.OK).body(responseCatalog);
+  }
+
+}
---
+++ src/main/java/com/example/catalogsservice/service/CatalogService.java
@@ -0,0 +1,31 @@
+package com.example.catalogsservice.service;
+
+import com.example.catalogsservice.dto.CatalogDto;
+import com.example.catalogsservice.jpa.CatalogEntity;
+import com.example.catalogsservice.jpa.CatalogRepository;
+import com.example.catalogsservice.vo.ResponseCatalog;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CatalogService {
+
+  private final CatalogRepository catalogRepository;
+
+  public Iterable<CatalogEntity> getAllCatalogs() {
+    return catalogRepository.findAll();
+  }
+
+  public CatalogDto getCatalogByProductId(String productId) {
+    CatalogEntity catalogEntity = catalogRepository.findByProductId(productId);
+    return new ModelMapper().map(catalogEntity, CatalogDto.class);
+  }
+
+
+}
